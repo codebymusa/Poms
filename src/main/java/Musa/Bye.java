55f4afb9bde919fe76cf3455a7ddd011564eddff
wtc <wtc@pop-os.localdomain>
2021-08-30 11:36:15 +0200

    practice

--- src/main/java/Musa/Bye.java
+++
@@ -1,7 +0,0 @@
-package Musa;
-
-public class Bye {
-    public static void main(String[] f){
-        System.out.println("Bye Basil");
-    }
-}
--- src/main/java/Musa/Hello.java
+++
@@ -1,7 +0,0 @@
-package Musa;
-
-public class Hello{
-    public static void main(String[] f){
-        System.out.println("Hello Basil");
-    }
-}
