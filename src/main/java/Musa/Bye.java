8086bb7245aea356ab8358bac1d7d3d10af55eb1
wtc <wtc@pop-os.localdomain>
2021-08-30 10:52:59 +0200

    Building Pom files and being able to execute the program on the command line.

---
+++ src/main/java/Musa/Bye.java
@@ -0,0 +1,7 @@
+package Musa;
+
+public class Bye {
+    public static void main(String[] f){
+        System.out.println("Bye Basil");
+    }
+}
---
+++ src/main/java/Musa/Hello.java
@@ -0,0 +1,7 @@
+package Musa;
+
+public class Hello{
+    public static void main(String[] f){
+        System.out.println("Hello Basil");
+    }
+}
